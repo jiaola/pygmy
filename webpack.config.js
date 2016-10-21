15d715c8c2228dcc41571992fee4acc3b56e7fec
djiao <djiao@iu.edu>
2016-10-21 00:02:48 -0400

    Add HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: './components/App.jsx',
   output: {
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "dist"),
     publicPath: "/",
     filename: "bundle.js"
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '田字格',
+      template: 'index.html',
+      inject: 'body'
+    })
+  ],
   module: {
     loaders: [
       {
