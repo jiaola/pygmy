078cbf172986aeb01d91f987826cf3541f18565e
djiao <djiao@iu.edu>
2016-10-27 00:06:25 -0400

    final touch

--- components/NewChar.jsx
+++ components/NewChar.jsx
@@ -5,7 +5,7 @@ const NewChar = ({onChange}) => (
   <FormGroup row>
     <Label for='newChars' sm={2}>添加生字</Label>
     <Col sm={10}>
-      <Input type="text" id='newChars' onKeyUp={onChange} onChange={function(e){}} placeholder="请填写要打印的生字"/>
+      <Input type="text" id='newChars' onKeyUp={onChange} onChange={function(e){}} placeholder="填写要打印的生字，按输入键（Return）添加。"/>
     </Col>
   </FormGroup>
 )
