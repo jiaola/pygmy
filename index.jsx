796e677bc1c647586bad44d7ebb4d7c166c2bfce
jiaola <lanlanxu@Lanlans-MacBook-Pro.local>
2016-11-23 05:48:22 -0500

    set up react-router

--- index.jsx
+++ index.jsx
@@ -4,11 +4,19 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 import App from './components/App'
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
 let reactElement = document.getElementById('react')
+const history = syncHistoryWithStore(browserHistory, store)
+
 render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    reactElement
+  <Provider store={store}>
+    { /* Tell the Router to use our enhanced history */ }
+    <Router history={history}>
+      <Route path="/" component={App}>
+      </Route>
+    </Router>
+  </Provider>,
+  reactElement
 )
--- reducers/index.js
+++ reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import chars from './chars'
 import options from './options'
+import { routerReducer } from 'react-router-redux'
 
 const rootReducer = combineReducers({
   chars,
-  options
+  options,
+  routing: routerReducer
 })
 
 export default rootReducer
