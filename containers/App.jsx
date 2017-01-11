71736187358df850d1b9d7f1dab6fa609c5f4ee7
djiao <djiao@iu.edu>
2017-01-10 19:40:34 -0500

    remove example

--- containers/App.jsx
+++ containers/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link, browserHistory } from 'react-router'
 import { Container, Row, Col, FormGroup, Button, Label, Input } from 'reactstrap'
 import NavigationBar from '../components/shared/NavigationBar'
-import Example from '../components/shared/Example'
 
 const App = ({ children }) => (
   <Container>
