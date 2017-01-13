04ed3e8fefe6f2c596544a95030fa0c19844c33f
djiao <djiao@iu.edu>
2017-01-12 22:11:03 -0500

    change api url

--- actions/index.js
+++ actions/index.js
@@ -1,2 +1,2 @@
-export const API_ROOT = 'http://localhost:3000'
-//export const API_ROOT = 'http://pygmy-api.brickowls.com'
+//export const API_ROOT = 'http://localhost:3000'
+export const API_ROOT = 'http://pygmy-api.brickowls.com'
