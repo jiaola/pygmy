d012fe3213f9c5ac02d16095bd3037d9bdf76aef
Dazhi Jiao <djiao@iu.edu>
2017-01-20 11:28:16 -0500

    Turn off redux-logger for production

--- store/index.js
+++ store/index.js
@@ -1,6 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers'
-import logger from 'redux-logger'
+import createLogger from 'redux-logger'
 import thunk from 'redux-thunk'
 
-export default applyMiddleware(thunk, logger())(createStore)(rootReducer)
+let logger = createLogger({
+  predicate: () => process.env.NODE_ENV === 'development'
+})
+
+export default applyMiddleware(thunk, logger)(createStore)(rootReducer)
